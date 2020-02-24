048d938668ca2e6b33b566f01d027c4e72c6ea80
Dan Leech <dan@danleech.com>
2020-02-24 16:42:51 +0000

    Removes Corsham Science for now

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
-templates=( bathasu corsham-science microgenetics pharmaxo )
+templates=( bathasu microgenetics pharmaxo )
 resolutions=( 1024x768 1280x720 1280x800 1280x960 1280x1024 1366x768 1440x900 1600x900 1680x1050 1920x1080 1920x1200 2560x1440 3440x1440 3840x1600 3840x2160 5120x1440 5120x2160 5120x2880 6016x3384 7680x4320 )
 
 rm -r output
