c2ab040fff76d75c6c475171a8d100851a07f037
Dan Leech <dan@danleech.com>
2020-02-25 16:49:43 +0000

    Tidies up output directories

--- generate.sh
+++ generate.sh
@@ -1,20 +1,23 @@
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
 templates=( bathasu microgenetics pharmaxo )
-resolutions=( 1024x768 1280x720 1280x800 1280x960 1280x1024 1366x768 1440x900 1600x900 1680x1050 1920x1080 1920x1200 2560x1440 3440x1440 3840x1600 3840x2160 5120x1440 5120x2160 5120x2880 6016x3384 7680x4320 )
+resolutions=( 1024x768 1280x720 1280x800 1280x960 1280x1024 1366x768 1440x900 1600x900 1680x1050 1920x1080 1920x1200 2560x1440 3440x1440 3840x1600 3840x2160 5120x1440 )
 
-rm -r output
-mkdir -p output
-
-rm -r logs
+rm -rf logs
 mkdir -p logs
 
 for template in "${templates[@]}"
 do
+    rm -rf $template
+    mkdir -p $template
     for resolution in "${resolutions[@]}"
     do
-        chrome --headless --disable-gpu --crash-dumps-dir=logs --screenshot=output/$template-$resolution.png --window-size=${resolution/x/,} --default-background-color=0 $template.html
+        chrome --headless --disable-gpu --crash-dumps-dir=logs --screenshot=$template/background$resolution.jpg --window-size=${resolution/x/,} --default-background-color=0 $template.html
     done
 done
 
 rm -r logs
+
+mv bathasu Bath\ ASU
+mv microgenetics Microgenetics
+mv pharmaxo Pharmaxo
